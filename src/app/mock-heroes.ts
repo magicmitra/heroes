38431d56021993f34d061b5af100f63e8775a49f
erv <ervinmitra@gmail.com>
2019-09-12 15:53:56 -0700

    spread them cheeks

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Mirana' },
+    { id: 12, name: 'Luna' },
+    { id: 13, name: 'Earth Spirit' },
+    { id: 14, name: 'Ember Spirit' },
+    { id: 15, name: 'Storm Spirit' },
+    { id: 16, name: 'Juggernaut' },
+    { id: 17, name: 'Earthshaker' },
+    { id: 18, name: 'Doom Bringer' },
+    { id: 19, name: 'Night Stalker' },
+    { id: 20, name: 'Phantom Lancer' }
+];
