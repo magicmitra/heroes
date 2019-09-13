aaaa8df0ec68c0c3ef8909216dd2fb13809d46e9
erv <ervinmitra@gmail.com>
2019-09-13 09:38:55 -0700

    more cheek spreading

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,6 +9,10 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
